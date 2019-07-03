eb9ab5ce501168d219b579cbd84183cca6d6d1fe
rgalyeon <dmifilin@gmail.com>
2019-07-03 17:00:26 +0300

    Add: Disemvowel_Trolls [7kyu][Python]

---
+++ Python/7kyu/Disemvowel_Trolls.py
@@ -0,0 +1,26 @@
+import re
+
+"""
+Instructions:
+
+Trolls are attacking your comment section!
+
+A common way to deal with this situation is to remove all of the vowels from the trolls' comments,
+neutralizing the threat.
+
+Your task is to write a function that takes a string and return a new string with all vowels removed.
+
+For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+
+Note: for this kata y isn't considered a vowel.
+
+"""
+
+
+def disemvowel(string):
+    string = re.sub(r'[aeuio]', '', string, flags=re.IGNORECASE)
+    return string
+
+
+if __name__ == '__main__':
+    print(disemvowel("This website is for losers LOL!"))  # "Ths wbst s fr lsrs LL!".
