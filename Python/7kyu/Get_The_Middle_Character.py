c4e1142182f1582087f5ac2eea7e8faae06bad10
rgalyeon <dmifilin@gmail.com>
2019-07-04 17:12:35 +0300

    Add: middle characters [7kyu][Python]

---
+++ Python/7kyu/Get_The_Middle_Character.py
@@ -0,0 +1,39 @@
+"""
+Instructions:
+
+You are going to be given a word. Your job is to return the middle character of the word.
+If the word's length is odd, return the middle character. If the word's length is even, return the middle 2 characters.
+
+#Examples:
+
+Kata.getMiddle("test") should return "es"
+
+Kata.getMiddle("testing") should return "t"
+
+Kata.getMiddle("middle") should return "dd"
+
+Kata.getMiddle("A") should return "A"
+
+#Input
+
+A word (string) of length 0 < str < 1000 (In javascript you may get slightly more
+than 1000 in some test cases due to an error in the test cases).
+You do not need to test for this.
+This is only here to tell you that you do not need to worry about your solution timing out.
+
+#Output
+
+The middle character(s) of the word represented as a string.
+
+"""
+
+
+def get_middle(s):
+    length = len(s)
+    return s[length // 2 if length % 2 != 0 else length // 2 - 1: length // 2 + 1]
+
+
+if __name__ == '__main__':
+    print(get_middle("eerAAeds"))
+    print(get_middle("AA"))
+    print(get_middle("eerAeds"))
