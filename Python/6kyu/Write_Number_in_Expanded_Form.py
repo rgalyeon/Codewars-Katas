d9c6933db6567c472aeab7f19e430f785ba73324
rgalyeon <dmifilin@gmail.com>
2019-07-04 09:12:05 +0300

    Add: expanded_form [6kyu][Python]

---
+++ Python/6kyu/Write_Number_in_Expanded_Form.py
@@ -0,0 +1,23 @@
+"""
+Instructions:
+
+You will be given a number and you will need to return it as a string in Expanded Form. For example:
+
+expanded_form(12) # Should return '10 + 2'
+expanded_form(42) # Should return '40 + 2'
+expanded_form(70304) # Should return '70000 + 300 + 4'
+
+NOTE: All numbers will be whole numbers greater than 0.
+
+"""
+
+
+def expanded_form(num):
+    num = list(str(num))
+    return " + ".join(n + "0" * (len(num) - i - 1) for i, n in enumerate(num) if n != "0")
+
+
+if __name__ == '__main__':
+    print(expanded_form(12))  # Should return '10 + 2'
+    print(expanded_form(42))  # Should return '40 + 2'
+    print(expanded_form(70304))  # Should return '70000 + 300 + 4'
